0dcef580352a497dac6fe38900fdafa82202842d
oivchenko <oivchenko@gmail.com>
2012-05-27 09:43:46 +0300

    first commit

---
+++ Mvcef/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+using Mvcef.Db;
+
+namespace Mvcef.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            var db = new AgiContext();
+            var u = new Employer() { Id=1, Name = "hhhh" };
+            db.Employers.Add(u);
+            db.SaveChanges();
+            var z = db.Employers.ToList();
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mvcef/Db/Employer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvcef.Db
+{
+    public class Employer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Decimal Salary { get; set; }
+        public string Name2 { get; set; }
+    }
+}
